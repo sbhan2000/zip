284f1467cdcceff1502736297ab724391d3ea32c
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2024-04-04 16:45:29 +0200

    Add files via upload

---
+++ AarohiX/utils/inline/start.py
@@ -0,0 +1,37 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AarohiX import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_10"],                 url=f"https://t.me/DAD_E3DAM"),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
